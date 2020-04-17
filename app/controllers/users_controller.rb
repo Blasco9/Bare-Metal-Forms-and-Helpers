0dd44fee0016efce6bf58bef01b24bac42061d6b
Julian Blasco <blascoblasco9@gmail.com>
2020-04-17 18:51:35 -0300

    Add linter files and fix rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
